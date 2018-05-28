5f0f76c3d2eead98e3ff226dc1660d6ca580253e
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-05-28 18:56:24 +0200

    + SuperTilemapEditor to Unity Tilemap converter

---
+++ Assets/TilemapConverter/TileIndexMap.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileIndexMap : ScriptableObject {
+    public TileBase[] Tiles;
+}
