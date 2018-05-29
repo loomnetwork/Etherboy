fbb09be1e09410b425a70c24dc4d7f30b13336c1
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-05-29 18:20:57 +0200

    Removed SuperTilemapEditor

--- Assets/CreativeSpore/SuperTilemapEditor/Scripts/Tilemap/SortingLayerAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace CreativeSpore.SuperTilemapEditor 
-{
-	public class SortingLayerAttribute : PropertyAttribute { }
-}
--- Assets/TilemapConverter/TileIndexMap.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-public class TileIndexMap : ScriptableObject {
-    public TileBase[] Tiles;
-}
