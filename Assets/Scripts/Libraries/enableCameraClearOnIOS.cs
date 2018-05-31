0ccaad078e7b49fc41c1644b6e891b97b57cf505
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-05-31 12:23:32 +0200

    Enabled cameras clearing for iOS

---
+++ Assets/Scripts/Libraries/enableCameraClearOnIOS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableCameraClearOnIOS : MonoBehaviour {
+	private void OnEnable () {
+#if UNITY_IOS
+	    // For some reason, on iOS camera outputs nothing if clear flags are not set
+	    GetComponent<Camera>().clearFlags = CameraClearFlags.Color;
+#endif
+	}
+}
