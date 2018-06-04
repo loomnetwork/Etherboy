7126468659759bf77d9a440504259597b9f3a562
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-06-04 11:40:31 +0200

    Fixed enableCameraClear

--- Assets/Scripts/Libraries/enableCameraClear.cs
+++ Assets/Scripts/Libraries/enableCameraClear.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enableCameraClearOnIOS : MonoBehaviour {
+public class enableCameraClear : MonoBehaviour {
 	private void OnEnable () {
-#if UNITY_IOS
-	    // For some reason, on iOS camera outputs nothing if clear flags are not set
+#if UNITY_IOS || UNITY_ANDROID
+	    // For some reason, on some platforms camera outputs nothing if clear flags are not set
 	    GetComponent<Camera>().clearFlags = CameraClearFlags.Color;
 #endif
 	}
